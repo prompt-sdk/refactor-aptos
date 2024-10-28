ee0f02631eb396831b675237a88b1d6001fee272
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-28 22:54:40 +0700

    update

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -1,27 +1,49 @@
-import { convertToCoreMessages, Message, streamText } from 'ai';
+import { convertToCoreMessages, Message, streamText, generateId } from 'ai';
 import { z } from 'zod';
-
+import { getAptosClient } from '@/components/utils/utils';
 import { customModel } from '@/ai';
 import { auth } from '@/app/(auth)/auth';
-import { deleteChatById, getChatById, saveChat , getTools} from '@/db/queries';
+import { deleteChatById, getChatById, saveChat, getTools } from '@/db/queries';
 import { Agent } from '@/db/schema';
 
 import { Model, models } from '@/lib/model';
 import { Tool } from '@langchain/core/tools';
 
+export const aptosClient = getAptosClient();
+
+export const zodExtract = (type: any, describe: any) => {
+  if (type == 'u128') return z.number().describe(describe);
+  if (type == 'u64') return z.number().describe(describe);
+  if (type == 'u8') return z.number().describe(describe);
+  if (type == 'bool') return z.boolean().describe(describe);
+  if (type == 'address') return z.string().describe(describe);
+  if (type == 'vector<u8>') return z.string().describe(describe);
+  if (type == 'vector<address>') return z.array(z.string()).describe(describe);
+  if (type == 'vector<string::String>')
+    return z.array(z.string()).describe(describe);
+  if (type == '0x1::string::String')
+    return z.array(z.string()).describe(describe);
+  if (type == 'generic')
+    return z.string().describe(' address type like 0x1::ABC::XYZ');
+  if (type == 'Type')
+    return z.string().describe(' address type like 0x1::ABC::XYZ');
+  if (type == 'TypeInfo')
+    return z.string().describe(' address type like 0x1::ABC::XYZ');
+  return z.string().describe(describe);
+};
 export async function POST(request: Request) {
   const {
     id,
     messages,
     model,
     agent,
-    tools
+    tools,
   }: {
     id: string;
     messages: Array<Message>;
     model: Model['name'];
     agent: Agent;
-    tools:Tool[];
+    tools: Tool[];
   } = await request.json();
 
   const session = await auth();
@@ -34,8 +56,53 @@ export async function POST(request: Request) {
     return new Response('Model not found', { status: 404 });
   }
   console.log(tools);
+  const toolData = tools.reduce((tool: any, item: any) => {
+    if (item.typeName == 'contractTool') {
+      const filteredObj = Object.keys(item.tool.params).reduce(
+        (acc: any, key: any) => {
+          acc[key] = key = zodExtract(
+            item.tool.params[key].type,
+            item.tool.params[key].description
+          );
+          return acc;
+        },
+        {}
+      );
+      const ParametersSchema: any = Object.fromEntries(
+        Object.entries(filteredObj).filter(
+          ([key, value]) => value !== undefined
+        )
+      );
+      type ParametersData = z.infer<typeof ParametersSchema>;
+      //if enty
+
+      tool[item.typeName + '_' + generateId()] = {
+        description: item.tool.description,
+        parameters: z.object(ParametersSchema),
+        execute: async (ParametersData: ParametersData) => {
+          const data = {
+            functionArguments: Object.values(ParametersData).map((item: any) =>
+              typeof item === 'number' ? BigInt(item * 10 ** 18) : item
+            ),
+            function: item.name,
+            typeArguments: item.tool.generic_type_params,
+          };
+          if (item.typeFunction == 'entry') {
+            return data;
+          }
+          if (item.typeFunction == 'view') {
+            const res = await aptosClient.view({ payload: data });
+
+            return res;
+          }
+        },
+      };
+      //if view return data
+      return tool;
+    }
+  });
   const coreMessages = convertToCoreMessages(messages);
- 
+
   const result = await streamText({
     model: customModel(model),
     system: `Your name is ${agent.name} \n\n
@@ -43,23 +110,7 @@ export async function POST(request: Request) {
       ${agent.prompt}`,
     messages: coreMessages,
     maxSteps: 5,
-    tools: {
-      getWeather: {
-        description: 'Get the current weather at a location',
-        parameters: z.object({
-          latitude: z.number(),
-          longitude: z.number(),
-        }),
-        execute: async ({ latitude, longitude }) => {
-          const response = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m&daily=sunrise,sunset&timezone=auto`
-          );
-
-          const weatherData = await response.json();
-          return weatherData;
-        },
-      },
-    },
+    tools: toolData,
     onFinish: async ({ responseMessages }) => {
       if (session.user && session.user.id) {
         try {
@@ -67,7 +118,7 @@ export async function POST(request: Request) {
             id,
             messages: [...coreMessages, ...responseMessages],
             userId: session.user.id,
-            agentId: agent.id
+            agentId: agent.id,
           });
         } catch (error) {
           console.error('Failed to save chat');
--- components/utils/utils.ts
+++ components/utils/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { Aptos, AptosConfig, Network } from '@aptos-labs/ts-sdk';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+
+export function getAptosClient() {
+  const config = new AptosConfig({ network: Network.TESTNET });
+  return new Aptos(config);
 }
