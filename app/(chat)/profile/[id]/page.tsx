3ee8405590a12ee0d3b4aa3241eb322c5643ffe5
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-29 18:12:59 +0700

    update

--- app/(chat)/profile/[id]/page.tsx
+++ app/(chat)/profile/[id]/page.tsx
@@ -4,8 +4,8 @@ import ProfileWidget from '@/components/custom/profile-widget';
 import { getUser } from '@/db/queries';
 
 const Profile = async ({ params }: { params: any }) => {
-  const { address } = await params;
-  const [user] = await getUser(address)
+  const { id } = await params;
+  const [user] = await getUser(id)
   //get userId by username ;
   return (
     <div className={'flex w-full grow items-center justify-center py-4'}>
