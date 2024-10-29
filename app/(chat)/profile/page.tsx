5afee8a0aa463a76574c1601be274f96862b6660
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-29 20:23:39 +0700

    update

---
+++ app/(chat)/profile/page.tsx
@@ -0,0 +1,21 @@
+import { cookies } from 'next/headers';
+import { auth } from '@/app/(auth)/auth';
+
+import DashboardProfile from '@/components/custom/dashboard-profile';
+import DashboardWidget from '@/components/custom/dashboard-widget';
+import { WidgetSelectionModal } from '@/components/custom/widget-selection-modal';
+
+
+export default async function Page(props: { searchParams: Promise<any> }) {
+  const session: any = await auth()
+  const user = session.user;
+
+  return (
+    <div className={'flex w-full grow items-center justify-center py-4'}>
+      <div className="container flex flex-col items-center justify-center gap-6">
+        <DashboardProfile user={user} />
+        <DashboardWidget user={user} />
+        <WidgetSelectionModal user={user} />
+      </div>
+    </div>);
+}
